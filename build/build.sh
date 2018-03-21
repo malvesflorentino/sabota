25abfb57f4818a21c86be5a59153a33b9aa151d0
Amanda Quinto <mandaquinto@gmail.com>
2018-03-21 12:09:41 -0300

    Alterando arquivos para o k8s

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 docker rm -f sabota
 docker rmi -f sabota
 docker build ../ -f ../Dockerfile -t sabota
-docker run --name=mano -d sabota
+docker run --name=sabota -d sabota
 docker ps
-docker tag sabota docker.io/mandalvesq/sabota
-docker push docker.io/mandalvesq/sabota
+docker tag sabota docker.io/vivareal/sabota
+docker push docker.io/vivareal/sabota
